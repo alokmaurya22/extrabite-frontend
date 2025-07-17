9d8cd54099abedef9b40dd2d6b2af0760832fe68
Alok Maurya <alokmauryaraj2003@gmail.com>
2025-07-17 23:45:59 +0530

    Resolved merge conflict in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-=======
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
->>>>>>> 5691d6b0dbe2d560fcf2b474984623a0e111bd8e
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-<<<<<<< HEAD
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
-=======
-}
->>>>>>> 5691d6b0dbe2d560fcf2b474984623a0e111bd8e
