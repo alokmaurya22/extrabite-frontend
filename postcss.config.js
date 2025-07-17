869954f4dee00719420607d882f39241a72fdefb
Alok Maurya <alokmauryaraj2003@gmail.com>
2025-07-17 23:47:01 +0530

    Resolved merge conflict in tailwind.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
 export default {
-  plugins: [
-    tailwindcss(),
-    autoprefixer(),
-  ],
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 }
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
->>>>>>> 5691d6b0dbe2d560fcf2b474984623a0e111bd8e
